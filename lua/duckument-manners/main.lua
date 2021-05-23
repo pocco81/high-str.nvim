de85e6a876a412de39d5e319b7c14dfaf30eb1ff
kdav5758 <kdav5758@gmail.com>
2021-05-22 23:26:11 -0500

    added dummy echoes for testing

--- lua/duckument-manners/main.lua
+++ lua/duckument-manners/main.lua
@@ -11,7 +11,17 @@ function M.main(option, command_option)
 	command_option = command_option or "NONE"
 
 	if (option == 0) then
-		cmd("echo 'Aligning document to the "..tostring(command_option).."...'")
+		cmd("echo 'Align - "..tostring(command_option).."...'")
+	elseif (option == 1) then
+		cmd("echo 'Pencil - "..tostring(command_option).."...'")
+	elseif (option == 2) then
+		cmd("echo 'Highlight - "..tostring(command_option).."...'")
+	elseif (option == 3) then
+		cmd("echo 'Bookmark - "..tostring(command_option).."...'")
+	elseif (option == 4) then
+		cmd("echo 'Correction - "..tostring(command_option).."...'")
+	elseif (option == 5) then
+		cmd("echo 'Repetition - "..tostring(command_option).."...'")
 	else
 		cmd("echo 'Command was not recognized for DuckumentManners.nvim'")
 	end
