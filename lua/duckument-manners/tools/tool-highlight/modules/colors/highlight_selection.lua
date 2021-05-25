c9f3ed196bc974f2753609a5187dfec87e179dfa
kdav5758 <kdav5758@gmail.com>
2021-05-24 21:38:11 -0500

    removed extra comments

--- lua/duckument-manners/tools/tool-highlight/modules/colors/highlight_selection.lua
+++ lua/duckument-manners/tools/tool-highlight/modules/colors/highlight_selection.lua
@@ -7,8 +7,6 @@ local cmd = vim.cmd
 
 function M.highlight_visual_selection(hi_group)
 
-	cmd("highlight LineHighlight ctermbg=green guibg=green")
-
 	api.nvim_exec([[
 		" Get the line and column of the visual selection marks
 		let [beg_line, beg_col] = getpos("'<")[1:2]
@@ -21,38 +19,29 @@ function M.highlight_visual_selection(hi_group)
 	local end_col = api.nvim_eval([[get(g:,"end_col", 0)]])
 	local current_buffer = api.nvim_eval([[bufnr('%')]])
 
-	-- cmd("echo 'beg_line: "..beg_line.."; beg_col: "..beg_col.."; end_line: "..end_line.."; end_col: "..end_col.."'")
 
 	if (beg_line == end_line) then
 		api.nvim_buf_add_highlight(current_buffer, 0, hi_group, beg_line - 1, beg_col, end_col)
-		-- api.nvim_buf_add_highlight(current_buffer, 0, 'LineHighlight', beg_line - 1, beg_col, end_col)
 	else
-		api.nvim_buf_add_highlight(current_buffer, 0, 'LineHighlight', beg_line - 1, beg_col, 2147483647)
+		api.nvim_buf_add_highlight(current_buffer, 0, hi_group, beg_line - 1, beg_col, 2147483647)
 		if (beg_line + 1 == end_line) then
 			api.nvim_buf_add_highlight(current_buffer, 0, hi_group, beg_line, 0, end_col)
-			-- api.nvim_buf_add_highlight(current_buffer, 0, 'LineHighlight', beg_line, 0, end_col)
 		elseif (beg_line - 1 == end_line) then
 			api.nvim_buf_add_highlight(current_buffer, 0, hi_group, beg_line - 1, 0, end_col)
-			-- api.nvim_buf_add_highlight(current_buffer, 0, 'LineHighlight', beg_line - 1, 0, end_col)
 		else
 			for i=beg_line - 1, end_line - 1, 1 do
 				if (i ~= end_line - 1) then
 					api.nvim_buf_add_highlight(current_buffer, 0, hi_group, i, 0, 2147483647)
-
-					-- api.nvim_buf_add_highlight(current_buffer, 0, 'LineHighlight', i, 0, 2147483647)
-					-- api.nvim_buf_add_highlight(current_buffer, 0, 'LineHighlight', i, 0, 2147483647)
 				else
 					api.nvim_buf_add_highlight(current_buffer, 0, hi_group, i, 0, end_col)
-
-					-- api.nvim_buf_add_highlight(current_buffer, 0, 'LineHighlight', i, 0, end_col)
-					-- api.nvim_buf_add_highlight(current_buffer, 0, 'LineHighlight', i, 0, end_col)
 				end
 			end
 		end
 	end
 
-	-- cmd([[let cursor_pos_after = getpos(".")]])
-	-- local after_pos_one
+	cmd([[let s:cursor_pos_after = getpos(".")]])
+	local after_pos_two = api.nvim_eval([[get(s:,"cursor_pos_after", 0)]])
+	cmd("echo 'after = "..after_pos_two.."'")
 	-- api.nvim_exec([[
 	-- 	let save_pos = getpos(".")
 	-- 	call nvim_buf_add_highlight(bufnr('%'), 0, 'LineHighlight', beg_minus_one, save_pos[2] - 1, save_pos[2])
