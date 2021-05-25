ae2d0babe45593122edb3f2282886a100e4d6c23
kdav5758 <kdav5758@gmail.com>
2021-05-24 23:01:44 -0500

    added column counter

--- lua/duckument-manners/tools/tool-highlight/modules/colors/highlight_selection.lua
+++ lua/duckument-manners/tools/tool-highlight/modules/colors/highlight_selection.lua
@@ -23,7 +23,7 @@ function M.highlight_visual_selection(hi_group)
 	if (beg_line == end_line) then
 		api.nvim_buf_add_highlight(current_buffer, 0, hi_group, beg_line - 1, beg_col, end_col)
 	else
-		api.nvim_buf_add_highlight(current_buffer, 0, hi_group, beg_line - 1, beg_col, 2147483647)
+		api.nvim_buf_add_highlight(current_buffer, 0, hi_group, beg_line - 1, beg_col, api.nvim_eval("col(["..beg_line..", '$']) - 1)"))
 		if (beg_line + 1 == end_line) then
 			api.nvim_buf_add_highlight(current_buffer, 0, hi_group, beg_line, 0, end_col)
 		elseif (beg_line - 1 == end_line) then
@@ -31,7 +31,7 @@ function M.highlight_visual_selection(hi_group)
 		else
 			for i=beg_line - 1, end_line - 1, 1 do
 				if (i ~= end_line - 1) then
-					api.nvim_buf_add_highlight(current_buffer, 0, hi_group, i, 0, 2147483647)
+					api.nvim_buf_add_highlight(current_buffer, 0, hi_group, i, 0, api.nvim_eval("col(["..i.." + 1, '$']"))
 				else
 					api.nvim_buf_add_highlight(current_buffer, 0, hi_group, i, 0, end_col)
 				end
@@ -39,9 +39,9 @@ function M.highlight_visual_selection(hi_group)
 		end
 	end
 
-	cmd([[let s:cursor_pos_after = getpos(".")]])
-	local after_pos_two = api.nvim_eval([[get(s:,"cursor_pos_after", 0)]])
-	cmd("echo 'after = "..after_pos_two.."'")
+	-- cmd([[let s:cursor_pos_after = getpos(".")]])
+	-- local after_pos_two = api.nvim_eval([[get(s:,"cursor_pos_after", 0)]])
+	-- cmd("echo 'after = "..after_pos_two.."'")
 	-- api.nvim_exec([[
 	-- 	let save_pos = getpos(".")
 	-- 	call nvim_buf_add_highlight(bufnr('%'), 0, 'LineHighlight', beg_minus_one, save_pos[2] - 1, save_pos[2])
