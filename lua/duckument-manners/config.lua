a71bb909102788b4dd35017bb7ad83069d28ab02
kdav5758 <kdav5758@gmail.com>
2021-05-25 09:19:42 -0500

    added opt to scan inner tables

--- lua/duckument-manners/config.lua
+++ lua/duckument-manners/config.lua
@@ -25,26 +25,28 @@ function config.set_options(opts)
     opts = opts or {}
 
     for opt, _ in pairs(opts) do
-
 		-- check if option exists in the config's table
 		if (config.options[opt] ~= nil) then		-- not nil
-
 			-- chec if option is a table
 			if (type(opts[opt]) == "table") then	-- if table
 				for inner_opt, _ in pairs(opts[opt]) do
-
-					-- table contains element by that key
-					if (config.options[opt][inner_opt] ~= nil) then		-- not nil
-						config.options[opt][inner_opt] = opts[opt][inner_opt]
-					else		-- nil
-						-- nothing
+					if (type(opts[opt][inner_opt]) == "table") then	-- if table
+						for inner_inner_opt, _ in pairs(opts[opt][inner_opt]) do
+							-- table contains element by that key
+							if (config.options[opt][inner_opt][inner_inner_opt] ~= nil) then		-- not nil
+								config.options[opt][inner_opt][inner_inner_opt] = opts[opt][inner_opt][inner_inner_opt]
+							end
+						end
+					else
+						-- table contains element by that key
+						if (config.options[opt][inner_opt] ~= nil) then		-- not nil
+							config.options[opt][inner_opt] = opts[opt][inner_opt]
+						end
 					end
 				end
-			else		-- if other
+			else
 				config.options[opt] = opts[opt]
 			end
-		else		-- nil
-			-- nothing
 		end
 	end
 end
