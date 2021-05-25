3ce9f61400f3eb57e31c6429439e7e22e4321595
kdav5758 <kdav5758@gmail.com>
2021-05-24 19:45:45 -0500

    fixed requiere stetements and added highlight func

--- lua/duckument-manners/tools/tool-highlight/service.lua
+++ lua/duckument-manners/tools/tool-highlight/service.lua
@@ -2,14 +2,16 @@
 
 local M = {}
 
-local color_parse = require("duckument-manners.tools.tool-highlight.modules.color_parser")
-local color_grabber = require("duckument-manners.tools.tool-highlight.modules.color_grabber")
+local color_parse = require("duckument-manners.tools.tool-highlight.modules.colors.color_parser")
+local color_grabber = require("duckument-manners.tools.tool-highlight.modules.colors.color_grabber")
+local highlight_selection = require("duckument-manners.tools.tool-highlight.modules.colors.highlight_selection")
 
 
 function M.highlight(hi_index)
 
 	color_parse.parse_colors()
 	hi_group = color_grabber.which_hi_group(hi_index)
+	highlight_selection.highlight_visual_selection(hi_group)
 
 end
 
