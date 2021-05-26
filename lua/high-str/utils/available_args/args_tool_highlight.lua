cc86d1ce61247ebfefd01feab1dd9d898ef732aa
kdav5758 <kdav5758@gmail.com>
2021-05-26 10:37:14 -0500

    moved table_length function to the utils and adde opt to give completion from more than 10 colors

--- lua/high-str/utils/available_args/args_tool_highlight.lua
+++ lua/high-str/utils/available_args/args_tool_highlight.lua
@@ -1,21 +1,16 @@
 
 
 local M = {}
+local arguments = {}
 
-local arguments = {
-	1,
-	2,
-	3,
-	4,
-	5,
-	6,
-	7,
-	8,
-	9,
-	0
-}
+local opts = require("high-str.config").options
+
 
 function M.available_commands()
+	for i=0, require("high-str.utils.general.tables").table_length(opts["highlight_colors"]), 1 do
+		table.insert(arguments, i)
+	end
+
 	return vim.tbl_keys(arguments)
 end
 
---
+++ lua/high-str/utils/general/tables.lua
@@ -0,0 +1,15 @@
+
+
+
+local M = {}
+
+function M.table_length(T)
+	local count = 0
+	for _ in pairs(T) do count = count + 1 end
+	return count
+end
+
+
+
+return M
+
