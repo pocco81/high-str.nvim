e8fe93eb2baaa6ac86f14cb9bf8c53b97a370b35
Pocco81 <pocco451@gmail.com>
2021-07-30 10:10:50 -0500

    formatted init

--- lua/high-str/init.lua
+++ lua/high-str/init.lua
@@ -1,12 +1,7 @@
-
-
 local M = {}
 
 function M.setup(custom_opts)
-	require("high-str.config").set_options(custom_opts)
+    require("high-str.config").set_options(custom_opts)
 end
 
-
-
 return M
-
