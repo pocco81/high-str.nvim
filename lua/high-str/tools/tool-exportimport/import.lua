80f04ce6ab856cf84be13b011f895460828d2a88
Pocco81 <pocco451@gmail.com>
2021-07-30 16:25:08 -0500

    fixed import

--- lua/high-str/tools/tool-exportimport/import.lua
+++ lua/high-str/tools/tool-exportimport/import.lua
@@ -35,16 +35,20 @@ function M.import()
 	local file = vim.fn.expand('%:p')
 
 	if (tbl_utils.has_element(file_cords or {}, file, "index") and file_cords ~= nil) then
-		for hi, _ in pairs(file_cords[file]) do
-			local hi_group = file_cords[file][hi]["hi_group"]
-			local beg_line = file_cords[file][hi]["beg_line"]
-			local beg_col = file_cords[file][hi]["beg_col"]
-			local end_line = file_cords[file][hi]["end_line"]
-			local end_col = file_cords[file][hi]["end_col"]
+		for _, vals in pairs(file_cords[file]) do
+			local hi_group = vals["hi_group"]
+			local beg_line = vals["beg_line"]
+			local beg_col = vals["beg_col"]
+			local end_line = vals["end_line"]
+			local end_col = vals["end_col"]
 
 			hl_selection.highlight_visual_selection(hi_group, beg_line, beg_col, end_line, end_col)
 
 		end
+
+		-- not sure why, but without this is doesn't work
+		-- it's silent becuase it will throw an error for not selecting anything
+		vim.cmd([[silent HSHighlight]])
 	else
 		print("HighStr: couldn't import highlights for current file.")
 	end
