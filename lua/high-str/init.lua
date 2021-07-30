9c3edfc7459787ed4767f8c1dd2a0692e0684504
Pocco81 <pocco451@gmail.com>
2021-07-30 11:38:57 -0500

    create saving dir

--- lua/high-str/init.lua
+++ lua/high-str/init.lua
@@ -2,15 +2,17 @@ local M = {}
 
 local opts = require("high-str.config").options
 local utils_paths = require("high-str.utils.general.paths")
-
-function M.setup(custom_opts)
-    require("high-str.config").set_options(custom_opts)
-end
+local fn = vim.fn
 
 local function create_saving_path()
     if (utils_paths.assert_dir(opts.saving_path) == 0) then
         fn.mkdir("" .. opts.saving_path .. "", "p")
     end
+end
+
+function M.setup(custom_opts)
+    require("high-str.config").set_options(custom_opts)
+	create_saving_path()
 end
 
 return M
